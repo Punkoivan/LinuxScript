ba7434db58be47ab172cfc433b887cf2d011bca8
Ivan <PunkoIvan>
2016-01-04 23:34:11 +0200

    Set device to var

--- screen_rotate.sh
+++ screen_rotate.sh
@@ -2,30 +2,32 @@
 
 # Find the line in "xrandr -q --verbose" output that contains current screen orientation, grep - select current orientation and assign value to var rotation;
 rotation="$(xrandr -q --verbose | grep 'connected' | egrep -o  '\) (normal|left|inverted|right) \(' | egrep -o '(normal|left|inverted|right)')"
+stylus="$xsetwacom --list devices | grep -o -e ".\+stylus""
 
+eraser="$xsetwacom --list devices | grep -o -e ".\+eraser""
 # Using current screen orientation proceed to rotate screen and input tools.
 case "$rotation" in
     normal)
     # rotate to the left
     xrandr -o left
-    xsetwacom set "Serial Wacom Tablet WACf004 stylus" rotate ccw
-    xsetwacom set "Serial Wacom Tablet WACf004 eraser" rotate ccw
+    xsetwacom set $stylus rotate ccw
+    xsetwacom set $eraser rotate ccw
     ;;
     left)
     # rotate to normal
     xrandr -o inverted
-    xsetwacom set "Serial Wacom Tablet WACf004 stylus" rotate half 
-    xsetwacom set "Serial Wacom Tablet WACf004 eraser" rotate half
+    xsetwacom set $stylus rotate half 
+    xsetwacom set $eraser rotate half
     ;;
     inverted)
  xrandr -o right
-    xsetwacom set "Serial Wacom Tablet WACf004 stylus" rotate cw
-    xsetwacom set "Serial Wacom Tablet WACf004 eraser" rotate cw
+    xsetwacom set $stylus rotate cw
+    xsetwacom set  $eraser rotate cw
 ;;
 right)
  xrandr -o normal
-    xsetwacom set "Serial Wacom Tablet WACf004 stylus" rotate none
-    xsetwacom set "Serial Wacom Tablet WACf004 eraser" rotate none
+    xsetwacom set $stylus rotate none
+    xsetwacom set  $eraser rotate none
 
 
 
