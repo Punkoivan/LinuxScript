1097aeee0dcbe181b26a49b0cf629c5e18bf6df1
Ivan <PunkoIvan>
2016-01-19 02:10:14 +0200

    trnlst ?

--- yandex_translate.sh
+++ yandex_translate.sh
@@ -2,5 +2,5 @@
 # Translate text with Yandex API
 text=`xclip -o`
 translate=en-ru
-  wget -O -  https://translate.yandex.net/api/v1.5/tr.json/translate --post-data="key=trnsl.1.1.20160117T231050Z.2d9e215cfc85a1fd.e529754def6a2c685b8efd67755bf1d4e82244de&lang=${translate}&text=${text}" | grep -o -e "\[.\+\]"| xargs  notify-send 
+  wget -O -  https://translate.yandex.net/api/v1.5/tr.json/translate --post-data="key=trnsl.1.1.20160117T231050Z.2d9e215cfc85a1fd.e529754def6a2c685b8efd67755bf1d4e82244de&lang=en-ru&text=${text}" | grep -o -e "\[.\+\]"| xargs  notify-send 
 
