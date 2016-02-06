3b6134ce2feb718d3fc0d382007523b61fb98520
Ivan <PunkoIvan>
2016-02-07 01:01:24 +0200

    output small chng

--- yandex_translate.sh
+++ yandex_translate.sh
@@ -12,5 +12,5 @@ translate=en-ru
 else
 translate=$1
 fi
-  wget -O -  https://translate.yandex.net/api/v1.5/tr.json/translate --post-data="key=trnsl.1.1.20160117T231050Z.2d9e215cfc85a1fd.e529754def6a2c685b8efd67755bf1d4e82244de&lang=${translate}&text=${text}" | grep -o -e "\[.\+\]" | zenity --text-info --width=450 --height=250 --title="Перевод \"${text}\""
+  wget -O -  https://translate.yandex.net/api/v1.5/tr.json/translate --post-data="key=trnsl.1.1.20160117T231050Z.2d9e215cfc85a1fd.e529754def6a2c685b8efd67755bf1d4e82244de&lang=${translate}&text=${text}" | grep -o -e "\[.\+\]" | zenity --text-info --width=450 --height=250 --title="Перевод для \"${text}\""
 
